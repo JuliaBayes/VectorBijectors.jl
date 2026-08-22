3a824dea6c1cc955230ed77201fba046a44bfcea
Penelope Yong <penelopeysm@gmail.com>
2026-08-22 22:29:35 +0100

    Disable Distributions tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,13 +10,13 @@ if !Sys.iswindows()
 end
 
 @testset "Plaice.jl" begin
-    include("distributions/univariate.jl")
-    include("distributions/multivariate.jl")
-    include("distributions/matrix.jl")
-    include("distributions/reshaped.jl")
-    include("distributions/cholesky.jl")
-    include("distributions/order.jl")
-    include("distributions/product.jl")
+    # include("distributions/univariate.jl")
+    # include("distributions/multivariate.jl")
+    # include("distributions/matrix.jl")
+    # include("distributions/reshaped.jl")
+    # include("distributions/cholesky.jl")
+    # include("distributions/order.jl")
+    # include("distributions/product.jl")
 
     include("pm/univariate.jl")
 end
